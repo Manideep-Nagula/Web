779e352bae88fb8251ef11d40d5a107ea3c04f7d
Manideep-Nagula <80896311+Manideep-Nagula@users.noreply.github.com>
2023-02-05 22:01:32 +0530

    Add files via upload

---
+++ siteverfication/Home/activites.php
@@ -0,0 +1,112 @@
+<!DOCTYPE hmtl>
+<html lang='en'>
+
+<head>
+    <title>School site</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="activity.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="header-class">
+                <div class="college-logo">
+                    <img class="logo" src="images/logo.jpg" alt="Institute logo">
+                </div>
+                <div class="college-title">
+                    <img class="logo" src="images/collegename.png" alt="college_title">
+                </div>
+            </div>
+        </header>
+
+        <nav>
+            <div class="container">
+                <ul>
+                    <li><a href="home.php">Home</a>
+                    <li><a href="activites.php">Activites</a></li>
+                    <li><a href="/website/index.php">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <section id="main">
+            <div class="container">
+                <article id="main-content">
+                    <h2>Institue Activities:</h2>
+                    <hr>
+                    <p>>> N.C.C. Training</p>
+                    <p>>> Technical Education Facility</p>
+                    <p>>> Eco – friendly club for environment awareness</p>
+                    <p>>> Tech Competitions</p>
+                    <p>>> Science Exhibition</p>
+                    <p>>> Alumini Guest Lectures</p>
+                    <div class="display-images">
+                        <img  src="images/acv1.jpg" alt="address">
+                    </div>
+                    <div class="display-images">
+                        <img  src="images/acv2.jfif" alt="address">
+                    </div>
+                    <br>
+                    <h2>Extra – curriculum Activities:</h2>
+                    <hr>
+                    <P>>>Institute Gatherings</P>
+                    <p>>>Sports Competitions</p>
+                    <p>>>Art and Technical Clubs</p>
+                    <div class="display-images">
+                        <img src="images/acv4.jpg" alt="address">
+                    </div>
+                    <div class="display-images">
+                        <img src="images/acv3.png" alt="address">
+                    </div>
+                    <div class="display-images">
+                        <img src="images/acv5.jfif" alt="address">
+                    </div>
+                   <br>
+                </article>
+
+                <aside> 
+                    <h2>DEPARTMENTS:</h2>
+                    <ul>
+                        <li><a href="#">Computer Science and Engineering</a></li>
+                        <li><a href="#">Department of Mathamatics</a></li>
+                        <li><a href="#">Department of Chemical Science</a></li>
+                        <li><a href="#">Department of Mathamatics</a></li>
+                        <li><a href="#">Department of Mechanical Engineering</a></li>
+                        <li><a href="#">Department of Civil Engineering </a></li>
+                        <li><a href="#">Department of Information Technology</a></li>
+                        <li><a href="#">Department of Chemical Engineering</a></li>
+                        <li><a href="#">Department of Electrical Engineering</a></li>
+                        <li><a href="#">Department of Electronics and Communication Engineering</a></li>
+                        <li><a href="#">Department of Metallurgy and Mining Engineering</a></li>
+                    </ul><br>
+                    <div class="contact-us">
+                        <h2>Contact us:</h2>
+                        <a href="tel:987456321">Call Us</a>
+                        <a href="mailto:abc@gamil.com">Mail Us</a>
+                        <h2>Address</h2>
+                        <p>Kukatpally,
+                        <br>Hyderabad-500002</p>
+                    </div>
+                </aside>
+
+
+            </div>
+
+        </section>
+
+        <footer>
+            <div class="container">
+                <a href="https://www.google.com"><img><img src="images/icons8-google-48.png"></a>
+                <a href="https://www.facebook.com"><img src="images/icons8-facebook-48.png"></a>
+                <a href="https://www.youtube.com"><img src="images/icons8-play-button-48.png"></a>
+                <p>&copy;2021 School ABC. All Right Reserved | Designed &amp; Developed By : Manideep Nagula & Bhuvana Chandra</p>
+            </div>
+
+    </div>
+    </footer>
+    </div>
+</body>
+
+</html>
---
+++ siteverfication/Home/home.php
@@ -0,0 +1,120 @@
+<!DOCTYPE hmtl>
+<html lang='en'>
+
+<head>
+    <title>School site</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="header-class">
+                <div class="college-logo">
+                    <img class="logo" src="images/logo.jpg" alt="Institute logo">
+                </div>
+                <div class="college-title">
+                    <img class="logo" src="images/collegename.png" alt="college_title">
+                </div>
+            </div>
+        </header>
+
+        <nav>
+            <div class="container">
+                <ul>
+                    <li><a href="home.php">Home</a>
+                    <li><a href="activites.php">Activites</a></li>
+                    <li><a href="/website/index.php">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <section id="main">
+            <div class="container">
+                <article id="main-content">
+                    <br>
+                    <h1> ABC Institute of Engineering, Hyderabad</h1>
+                    <p class="table-wrapper">
+                    <marquee> Education is the passport to the future, for tomorrow belongs to those who prepare for it today.
+                         #Stay Home...#Stay Safe...
+                    </marquee></p>
+                    <hr>
+
+                    <div class="table-wrapper">
+                        <img id = "logo3" src="images/main.jpg" alt="School"></div>
+                    <p>
+                        Established in the 1990's , the Institue of Engineering, Hyderabad has been a profound
+                        educational institution in the teaching of engineering methods and sciences and has 
+                        been a hub of technological advances.
+                    </p>
+                    <p>Teaching engineering is teaching the application of scientific, economic, social, 
+                    and practical knowledge in order to design, build, maintain, and improve structures, 
+                    machines, devices, systems, materials and processes. It may encompass using insights 
+                    to conceive, model and scale an appropriate solution to a problem or objective. The 
+                    discipline of engineering is extremely broad, and encompasses a range of more specialized
+                    fields of engineering, each with a more specific emphasis on particular areas of technology 
+                    and types of application. Engineering disciplines include aerospace, biological, civil,
+                    chemical, computer, electrical, industrial and mechanical.</p>
+                    <br>
+                    <h2>Available facilities:</h2>
+                    <hr>
+                    <p>1) Technical Education </p>
+                    <p>2) N.C.C.</p>
+                    <p>3) Interview Training</p>
+                    <P>4) Competitive Preparation</P>
+                    <p>5) Entrepreneuship Opportunities</p>
+                    <h2>We Offer:</h2>
+                    <hr>
+                    <p>1) Good Infrastructure </p>
+                    <p>2) High-tech Labratories</p>
+                    <p>3) Library</p>
+                    <P>4) Sports Grounds</P>
+                    <P>5) Health Care Centre</P>
+                    <br>
+
+                </article>
+
+                <aside> 
+                    <h2>DEPARTMENTS:</h2>
+                    <ul>
+                        <li><a href="#">Computer Science and Engineering</a></li>
+                        <li><a href="#">Department of Mathamatics</a></li>
+                        <li><a href="#">Department of Chemical Science</a></li>
+                        <li><a href="#">Department of Mathamatics</a></li>
+                        <li><a href="#">Department of Mechanical Engineering</a></li>
+                        <li><a href="#">Department of Civil Engineering </a></li>
+                        <li><a href="#">Department of Information Technology</a></li>
+                        <li><a href="#">Department of Chemical Engineering</a></li>
+                        <li><a href="#">Department of Electrical Engineering</a></li>
+                        <li><a href="#">Department of Electronics and Communication Engineering</a></li>
+                        <li><a href="#">Department of Metallurgy and Mining Engineering</a></li>
+                    </ul><br>
+                    <div class="contact-us">
+                        <h2>Contact us:</h2>
+                        <a href="tel:987456321">Call Us</a>
+                        <a href="mailto:abc@gamil.com">Mail Us</a>
+                        <h2>Address</h2>
+                        <p>Kukatpally,
+                        <br>Hyderabad-500002</p>
+                    </div>
+                </aside>
+            </div>
+
+        </section>
+
+        <footer>
+            <div class="container">
+                <a href="https://www.google.com"><img><img src="images/icons8-google-48.png"></a>
+                <a href="https://www.facebook.com"><img src="images/icons8-facebook-48.png"></a>
+                <a href="https://www.youtube.com"><img src="images/icons8-play-button-48.png"></a>
+                <p>&copy;2021 School ABC. All Right Reserved | Designed &amp; Developed By : Manideep Nagula & Bhuvana Chandra</p>
+            </div>
+
+    </div>
+    </footer>
+    </div>
+</body>
+
+</html>
